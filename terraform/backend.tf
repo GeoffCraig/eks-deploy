5805fe653385e262c3d06d3976541ecfdb5bb196
Geoff Craig <geoff.craig@hotmail.com>
2022-05-04 22:10:24 -0500

    testing creation of a VPC

---
+++ terraform/backend.tf
@@ -0,0 +1,7 @@
+backend "s3" {
+    bucket = "terraform-state-builder-9999"
+    key = "dev/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = "true"
+    dynamodb_table = "terraform_state_builder_lock"
+}
