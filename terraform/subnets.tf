2ae303a56e7e253cd48a13c439ec3d67db770e9e
Geoff Craig <geoff.craig@hotmail.com>
2022-05-12 17:14:50 -0500

    fixing a typo

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -2,6 +2,6 @@ resource "aws_subnet" "eks_subnets" {
     for_each = var.subnets
     
     vpc_id = aws_vpc.eks_dev_vpc.id
-    cidr_block = each.cidr
-    availability_zone = each.az
+    cidr_block = each.value.cidr
+    availability_zone = each.value.az
 }
