2f5865fca20d28b83fb24b97c852f79116468d93
Geoff Craig <geoff.craig@hotmail.com>
2022-05-20 15:58:08 -0500

    Route table testing

---
+++ terraform/route_table.tf
@@ -0,0 +1,14 @@
+resource "aws_route_table" "route_table" {
+  for_each = aws_subnet.eks_subnets
+  vpc_id = aws_vpc.eks_dev_vpc
+  route = {
+    cidr_block = each.value.cidr
+  }
+  route = {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway[each.key].id 
+  }
+  tags = {
+    Name = each.key
+  }
+}
