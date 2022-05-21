7e0b12c641137c2b59121bb642b72ceb00e8b438
Geoff Craig <geoff.craig@hotmail.com>
2022-05-21 12:36:24 -0500

    route table

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -3,8 +3,8 @@ resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.eks_dev_vpc
   route = [
     {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway[each.key].id 
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_nat_gateway.nat_gateway[each.key].id 
     }
   ]
   tags = {
