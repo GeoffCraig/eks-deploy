f2f3607204ed6ac8076b2b4e6d7ec327098ed56e
Geoff Craig <geoff.craig@hotmail.com>
2022-05-21 12:45:32 -0500

    route table

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -1,12 +1,10 @@
 resource "aws_route_table" "route_table" {
   for_each = aws_subnet.eks_subnets
   vpc_id = aws_vpc.eks_dev_vpc
-  route = [
-    {
+  route = {
       cidr_block = "0.0.0.0/0"
-      gateway_id = aws_nat_gateway.nat_gateway[each.key].id 
+      nat_gateway_id = aws_nat_gateway.nat_gateway[each.key].id 
     }
-  ]
   tags = {
     Name = each.key
   }
